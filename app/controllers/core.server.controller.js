9a26b782b6e2b5c56c5a79e773621c9ffc057fcd
Derek Lin <derek.pc.lin@gmail.com>
2015-07-25 10:57:45 -0400

    Cleaned up server controller.

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -9,17 +9,6 @@ var Contact = require('../models/contact');
  * Module dependencies.
  */
 exports.index = function(req, res) {
-/*    var service1 = new Service({title: 'Web/UI Design', description: 'Design beautiful web/mobile interfaces that are visually engaging and simple to use.'});
-    var service2 = new Service({title: 'HTML/CSS', description: 'Utilizing HTML5, CSS3, JavaScript and RWD I build each project to meet the needs of the client.'});
-    var service3 = new Service({title: 'Research', description: 'Understanding the needs of each individual client and business is vital in deliverying the perfect user experience.'});
-
-
-    service1.save();
-    service2.save();
-    service3.save();
-*/
-    //Works.remove().exec();
-
     res.render('index', {
         description: "Derek Lin Portfolio Site"
     });
